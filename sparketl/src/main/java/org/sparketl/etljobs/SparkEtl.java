0cabb12457b5214d976b67762fe1c224509bde11
vijith.reddy <vijith.reddy@172.16.9.42>
2015-08-04 13:50:08 -0700

    Added RDD persist example

--- sparketl/src/main/java/org/sparketl/etljobs/SparkEtl.java
+++ sparketl/src/main/java/org/sparketl/etljobs/SparkEtl.java
@@ -8,6 +8,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.storage.StorageLevel;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
@@ -19,9 +20,9 @@ import scala.Tuple2;
  */
 public final class SparkEtl {
 	public static void main(String[] args) throws Exception {
-		if (args.length < 4) {
+		if (args.length < 5) {
 			System.err
-					.println("Please use: SparkEtl <master> <input file> <output file> <country>");
+					.println("Please use: SparkEtl <master> <input file> <output file for key value> <output file for values only> <country>");
 			System.exit(1);
 		}
 		// System.out.println("The class path is    "+System.getProperty("java.class.path"));
@@ -33,7 +34,7 @@ public final class SparkEtl {
 		JavaRDD<String> file = spark.textFile(args[1]);
 
 		// Should be a final variable for variable scope in Java8
-		final String filterByCt = (String) args[3];
+		final String filterByCt = (String) args[4];
 
 		// As per JSON file each line item is a different json
 		FlatMapFunction<String, String> jsonLine = jsonFile -> {
@@ -70,7 +71,14 @@ public final class SparkEtl {
 
 		// Reduce the result set as per the filter criteria and save it
 		JavaPairRDD<String, String> reduced = pairs.filter(func);
+		// Caching intermediate RDD for reuse
+		reduced.persist(StorageLevel.MEMORY_AND_DISK());
+		// Action to save the file to disk
 		reduced.saveAsTextFile(args[2]);
+		// Retrieve values leaving the keys behind
+		JavaRDD<String> maps = reduced.keys();
+		// Save the values to the disk
+		maps.saveAsTextFile(args[3]);
 		System.exit(0);
 
 	}
